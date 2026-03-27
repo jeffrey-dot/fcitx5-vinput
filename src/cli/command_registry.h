bc690e385ea4d52a0a76ad7d14e7408ac1c5c163
xifan2333 <xifan233@163.com>
2026-03-27 21:59:06 +0800

    Drop unused registry CLI commands

--- src/cli/command_registry.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "cli/utils/cli_context.h"
-#include "cli/utils/formatter.h"
-
-int RunRegistryStatus(Formatter &fmt, const CliContext &ctx);
-int RunRegistrySync(Formatter &fmt, const CliContext &ctx);
